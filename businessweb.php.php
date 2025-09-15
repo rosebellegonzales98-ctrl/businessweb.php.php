9e04474b99d79d88eedaf96039fd080c8315f80d
rosebellegonzales98-ctrl <rosebellegonzales98@gmail.com>
2025-09-15 20:45:08 +0800

    Add files via upload

---
+++ businessweb.php.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>businessweb</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body> 
+       <header>
+        <nav class="navbar section-content">
+            <nav class="navbar">
+                <a href="#" class="nav-logo">
+                    <h2 class="logo-text">🍰Tira-misu Cake</h2>
+                </a>
+                <ul class="nav-menu">
+                    <button id="menu-close-button" class="fas fa-times"></button>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                      <li class="nav-item">
+                        <a href="#about" class="nav-link">About</a>
+                    </li>
+                      <li class="nav-item">
+                        <a href="#menu" class="nav-link">Menu</a>
+                    </li>
+                      <li class="nav-item">
+                        <a href="#testimonials" class="nav-link">Testimonials</a>
+                    </li>
+                      <li class="nav-item">
+                        <a href="#gallery" class="nav-link">Gallery</a>
+                    </li>
+                      <li class="nav-item">
+                        <a href="#contact" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+                <button id="menu-open-button" class="fas fa-bars"></button>
+            </nav>
+        
+       </header>
+
+
+       <main>
+       <section class="hero-section">
+        <div class="section-content">
+            <div class="hero-details">
+                <h2 class="title">Best Tira-misu Cake</h2>
+                <h3 class="subtitle">Layered with Love, Dusted with Delight, Tiramisu Like Never Before!</h3>
+                <p class="description">Welcome to Tira-misu cake. Treat yourself to the rich, creamy delight of our authentic tiramisu cake.
+                    To celebrate your first visit, enjoy 15% off your order with code TREAT15. Indulge today and discover why our tiramisu 
+                    is the sweetest way to brighten your day!</p>
+
+                <div class="buttons">
+                    <a href="#" class="button order-now">Order Now</a>
+                    <a href="#" class="button contact-us">Contact Us</a>
+                </div>
+            </div>
+            <div class="hero-image-wrapper">
+                <img src="Picture2.png" alt="hero" class="hero-image">
+            </div>
+        </div>
+       </section>
+
+       <section class="about-section" id="about">
+        <div class="section-content">
+            <div class="about-image-wrapper">
+                <img src="tiramisu.jpg" alt="about" class="about-image">
+            </div>
+            <div class="about-details">
+                <h2 class="section-title">About Us</h2>
+                <p class="text">At Tira-misu cake, we’re passionate about crafting the perfect tiramisu layer by layer. 
+                    Using only the finest ingredients, our cakes blend rich espresso, creamy mascarpone, and delicate ladyfingers
+                     to create a dessert that’s both timeless and unforgettable. Whether you're celebrating a special occasion or 
+                     simply indulging, our tiramisu is made with love and authenticity in every bite. Born from tradition, perfected 
+                     with care this is tiramisu the way it should be</p>
+                     <div class="social-link-list">
+                        <a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-x-twitter"></i></a>
+                     </div>
+            </div>
+        </div>
+       </section>
+
+       <section class="menu-section" id="menu">
+        <h2 class="section-title">Our Menu</h2>
+        <div class="sectipn-content">
+            <ul class="menu-list">
+                <li class="menu-item">
+                    <img src="ube.png" alt="ube" class="menu-image">
+                    <h3 class="name">Ube Tira-misu Cake</h3>
+                    <p class="text">A dreamy fusion of creamy tiramisu and earthy ube 
+                        layered with ube sponge, mascarpone cream, 
+                        and a hint of espresso. A vibrant twist on an Italian classic!</p>
+                </li>
+                <li class="menu-item">
+                    <img src="strawberry.png" alt="strawberry" class="menu-image">
+                    <h3 class="name">Strawberry Tira-misu Cake</h3>
+                    <p class="text">A fruity take on the classic layers of strawberry-soaked sponge,
+                         mascarpone cream, and fresh strawberries. Light, luscious, and refreshingly 
+                         sweet.</p>
+                </li>
+                <li class="menu-item">
+                    <img src="Picture2.png" alt="Original" class="menu-image">
+                    <h3 class="name">Original Tira-misu Cake</h3>
+                    <p class="text">The timeless Italian favorite espresso-soaked sponge, rich
+                         mascarpone cream, and a dusting of cocoa. Bold, creamy, and irresistibly smooth</p>
+                </li>
+                <li class="menu-item">
+                    <img src="matcha.png" alt="matcha" class="menu-image">
+                    <h3 class="name">Matcha Tira-misu Cake</h3>
+                    <p class="text">East meets West in this elegant twist matcha-soaked sponge layered with velvety 
+                        mascarpone cream and finished with a fine dusting of premium green tea powder.</p>
+                </li>
+                <li class="menu-item">
+                    <img src="pistachio.png" alt="pistachio" class="menu-image">
+                    <h3 class="name">Pistachio Tira-misu Cake</h3>
+                    <p class="text">Nutty, creamy, and subtly sweet tiramisu with a pistachio punch.</p>
+                </li>
+                <li class="menu-item">
+                    <img src="blackberry.png" alt="blackberry" class="menu-image">
+                    <h3 class="name">Blackberry Tira-misu Cake</h3>
+                    <p class="text">A vibrant twist on tradition layers of blackberry soaked sponge, mascarpone cream, and bursts of fresh berry flavor.</p>
+                </li>
+            </ul>
+        </div>
+       </section>
+
+       <section class="testimonials-section" id="testimonials">
+        <h2 class="section-title">Testimonials</h2>
+        <div class="section-content">
+            <div class="slider-container swiper">
+                <div class="slider-wrapper">
+                    <ul class="testimonials-list swiper-wrapper">
+                        <li class="testimonial swiper-slide">
+                            <img src="pic1.jpg" alt="user" class="user-image">
+                            <h3 class="name">John Isaac Ocampo</h3>
+                            <i class="feedback">"Portion size was generous, enough to share! 
+                                I’d definitely order again, but maybe offer a smaller slice option 
+                                for those who don’t want something too heavy"</i>
+                        </li>
+                         <li class="testimonial swiper-slide">
+                            <img src="pic2.jpg" alt="user" class="user-image">
+                            <h3 class="name">Sabarina Kaaya Gonzales</h3>
+                            <i class="feedback">"Not my favorite. The espresso flavor was
+                                 too overpowering, and the cream didn’t taste as fresh as I expected."</i>
+                        </li>
+                         <li class="testimonial swiper-slide">
+                            <img src="pic3.jpg" alt="user" class="user-image">
+                            <h3 class="name">Chrysha Perez</h3>
+                            <i class="feedback">"Perfect balance of sweetness and bitterness. 
+                                I loved that it wasn’t too sweet just right with a cup of coffee."</i>
+                        </li>
+                         <li class="testimonial swiper-slide">
+                            <img src="pic4.jpg" alt="user" class="user-image">
+                            <h3 class="name">"Ashtee Dy Conception</h3>
+                            <i class="feedback">Really good overall, but I wish the cocoa powder on top was
+                                 a bit thicker to give a stronger chocolate taste."</i>
+                        </li>
+                         <li class="testimonial swiper-slide">
+                            <img src="pic5.jpg" alt="user" class="user-image">
+                            <h3 class="name">Reich Gabriel Santos</h3>
+                            <i class="feedback">"This tiramisu was heavenly! The cream was smooth and light, and the
+                                 coffee flavor wasn’t too bitter. Easily one of the best I’ve tried."</i>
+                        </li>
+                    </ul>
+            
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-slide-button swiper-button-prev"></div>
+                    <div class="swiper-slide-button swiper-button-next"></div>
+                </div>
+            </div>
+        </div>
+       </section>
+
+
+       <section class="gallery-section" id="gallery">
+        <h2 class="section-title">Gallery</h2>
+        <div class="section-content">
+            <ul class="gallery-list">
+                <li class="gallery-item">
+                    <img src="pics1.jpg" alt="original" class="gallery-image">
+                </li>
+                   <li class="gallery-item">
+                    <img src="pics2.jpg" alt="original" class="gallery-image">
+                </li>
+                   <li class="gallery-item">
+                    <img src="pics3.jpg" alt="original" class="gallery-image">
+                </li>
+                   <li class="gallery-item">
+                    <img src="pics4.jpg" alt="original" class="gallery-image">
+                </li>
+                   <li class="gallery-item">
+                    <img src="pics5.jpg" alt="original" class="gallery-image">
+                </li>
+                   <li class="gallery-item">
+                    <img src="pics6.jpg" alt="original" class="gallery-image">
+                </li>
+
+            </ul>
+        </div>
+       </section>
+
+       <section class="contact-section" id="contact">
+        <h2 class="section-title">Contact Us</h2>
+        <div class="section-content">
+            <ul class="contact-info-list">
+                <li class="contact-info">
+                    <i class="fa-solid fa-location-crosshairs"></i>
+                    <p> 123 Campsite Avenue,Wilderness, CA 98765</p>
+                </li>
+                 <li class="contact-info">
+                    <i class="fa-regular fa-envelope"></i>
+                    <p>info@tira-misucakewebsite.com</p>
+                </li>
+                 <li class="contact-info">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>(123) 456-78909</p>
+                </li>
+                 <li class="contact-info">
+                    <i class="fa-regular fa-clock"></i>
+                    <p>Monday -Saturday: 9:00 AM - 10:00 PM</p>
+                </li>
+                 <li class="contact-info">
+                    <i class="fa-regular fa-clock"></i>
+                    <p>Sunday: Closed</p>
+                </li>
+                 <li class="contact-info">
+                    <i class="fa-solid fa-globe"></i>
+                    <p>www.codingnepalweb.com</p>
+                </li>
+            </ul>
+
+            <form action="#" class="contact-form">
+                <input type="text" placeholder="Your Name" class="form-input" required>
+                <input type="email" placeholder="Your email" class="form-input" required>
+                <textarea placeholder="Your message" class="form-input" required></textarea>
+                <button class="submit-button">Submit</button>
+            </form>
+        </div>
+       </section>
+
+       <section class="footer-section">
+        <div class="section-content">
+            <p class="copyright-text">©2025 tira-misu cake</p>
+
+            <div class="social-link-list">
+                        <a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#" class="social-link"><i class="fa-brands fa-x-twitter"></i></a>
+                     </div>
+
+                     <p class="policy-text">
+                        <a href="#" class="policy-link">Privacy Policy</a>
+                        <span class="separator">•</span>
+                        <a href="#" class="policy-link">Refund Policy</a>
+                     </p>
+        </div>
+</section>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+<script src="script.js"></script>
+        </div>
+     </body>    
+     </html>
